0af30ff118eb2ad10031aa702968aae0c68a5969
MD ANAS UDDIN <rrabby74@gmail.com>
2026-09-12 09:48:17 +0600

    Update A1answer.js

--- A1answer.js
+++ A1answer.js
@@ -49,11 +49,11 @@ function getDayType(day) {
 
 //Question No. 3
 function validateUsername(username) {
-    if (username.includes(" ")) {
-        return "No Space Allowed";
-    }
-    else if (username.length < 4) {
+    if (username.length < 4) {
         return "Too Short";
+    }
+    else if (username.includes(" ")) {
+        return "No Space Allowed";
     }
     else if (username.toLowerCase().includes("admin")) {
         return "Reserved Word";
